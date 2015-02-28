9d551976c0efc0ce0275706743bf1edd8fdd2470
Chun-Wei Liu <chunwei@cs.unc.edu>
2015-02-28 15:03:19 -0500

    basic demo done

--- Img2Html.cc
+++ Img2Html.cc
@@ -4,26 +4,29 @@ void Img2Html::SetFilename(const std::string& filename) {
   filename_ = filename;
 }
 
-void Img2Html::SetList(const RankList& list) { list_ = list; }
+void Img2Html::SetLists(const std::vector<RankList>& lists) { lists_ = lists; }
 
-void Img2Html::PrintList(int height) {
+void Img2Html::PrintLists(int height) {
   std::ofstream file;
   file.open(filename_);
 
   // Building HTML
   file << "<!DOCTYPE html>" << std::endl;
   file << "<html><body>" << std::endl;
-  for (int i = 0; i < list_.size(); ++i) {
+  for (int i = 0; i < lists_[0].size(); ++i) {
     // one row in each loop
     file << "<div>" << std::endl;
 
-    // Print figure
-    file << "<img src=\"" << list_[i].first << "\""
-         << " height=\"" << height << "\">" << std::endl;
+    for (int j = 0; j < lists_.size(); ++j) {
+      // Print figure
+      file << "<img src=\"" << lists_[j][i].first << "\""
+           << " height=\"" << height << "\">" << std::endl;
 
-    // Print attributes
-    file << "Rank: " << i << std::endl;
-    file << "Score: " << list_[i].second << std::endl;
+      // Print attributes
+      file << "[" << i + 1 << "]:" << std::endl;  // rank
+      file << std::setprecision(3) << lists_[j][i].second
+           << std::endl;  // score
+    }
 
     file << "</div>" << std::endl;
   }
--- Img2Html.h
+++ Img2Html.h
@@ -2,6 +2,7 @@
 #define IMG_2_HTML_
 
 #include <fstream>
+#include <iomanip>
 #include <string>
 #include <vector>
 
@@ -10,13 +11,12 @@ typedef std::vector<std::pair<std::string, float> > RankList;
 class Img2Html {
  private:
   std::string filename_;
-  RankList list_;
+  std::vector<RankList> lists_;
 
  public:
-  void SetFilename(const std::string& filename);
-  void SetList(const RankList& list);
+  void SetFilename(const std::string&);
+  void SetLists(const std::vector<RankList>&);
 
-  void PrintList(int);
   void PrintLists(int);
 };
 
